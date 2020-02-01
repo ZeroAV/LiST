4f8cd94bbb2fd52817f07a7f334db19d84f2f3f2
Av <aditya.alvari28@gmail.com>
2020-02-01 21:52:07 +0700

    Initial Commit

---
+++ LiST/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LiST
+//
+//  Created by Aditya Alvari on 01/02/20.
+//  Copyright © 2020 Aditya Alvari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ LiSTTests/LiSTTests.swift
@@ -0,0 +1,34 @@
+//
+//  LiSTTests.swift
+//  LiSTTests
+//
+//  Created by Aditya Alvari on 01/02/20.
+//  Copyright © 2020 Aditya Alvari. All rights reserved.
+//
+
+import XCTest
+@testable import LiST
+
+class LiSTTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
